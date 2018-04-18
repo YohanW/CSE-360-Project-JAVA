b4c880c17433c63010ee27ef512842e99a431f1c
Alejandro Rayas <alexrayas@rocketmail.com>
2018-04-17 17:46:39 -0700

    Update NewFileChooser_Alejandro.java

--- NewFileChooser_Alejandro.java
+++ NewFileChooser_Alejandro.java
@@ -260,20 +260,20 @@ public class NewFileChooser_Alejandro extends javax.swing.JFrame implements Acti
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewFileChooser.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewFileChooser_Alejandro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewFileChooser.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewFileChooser_Alejandro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewFileChooser.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewFileChooser_Alejandro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewFileChooser.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewFileChooser_Alejandro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewFileChooser().setVisible(true);
+                new NewFileChooser_Alejandro().setVisible(true);
             }
         });
     }
